0150b75959bb7c0233e51f64b11e87735b8e8e00
soumyadeep70 <soumyadeepdash70@gmail.com>
2024-12-23 17:21:21 +0530

    23 dec#7

--- hosts/mercury/users/cypher/hyprland/environment-vars.nix
+++ hosts/mercury/users/cypher/hyprland/environment-vars.nix
@@ -3,11 +3,24 @@
 let 
   envFile = ".config/uwsm/env";
   env-hyprlandFile = ".config/uwsm/env-hyprland";
-  toEnvFormat = attrs: lib.concatStringsSep "\n" (map (name: "export ${name}='${attrs.${name}}'") (lib.attrNames attrs));
+  toEnvFormat = attrs:
+    let
+      mkEntry = key:
+        let
+          value = if lib.isString attrs.${key} then
+            "'${attrs.${key}}'"
+          else
+            toString attrs.${key};
+        in
+          "export ${key}=${value}";
+    in
+      lib.concatStringsSep "\n" (map mkEntry (lib.attrNames attrs));
 in
 {
   home.file.${envFile}.text = toEnvFormat {
     AMAZE = "hyprland";
     POOP = "sway";
+    SIZE = 28;
+    SCORE = 3.14;
   };
 }
